836fd8b2b3793e5c9fb6a56eb07f36bec471d91e
Roel <roelwinter11@gmail.com>
2021-06-30 16:22:43 +0200

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './Free vector anaconda snake.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="Cartoon snake" />
+        <p>
+          Snake game!
+        </p>
+      </header>
+
+      <footer className="App-footer">
+        <a href="https://www.vecteezy.com/free-vector/snake">Snake Vectors by Vecteezy</a>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
