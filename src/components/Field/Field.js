6a4d848d935a585109c3b7c1fa23aced9d134e4d
Roel <roelwinter11@gmail.com>
2021-07-09 17:33:22 +0200

    Make snakehead move in specified direction by keypress

--- src/components/Field/Field.js
+++ src/components/Field/Field.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './Field.css';
 
 
@@ -8,21 +8,44 @@ const defaultTileProps = {
 }
 
 const amountOfRows = 10;
-const amountOfColumns = amountOfRows - 2;
+const amountOfColumns = amountOfRows;
 const food = String.fromCodePoint(0x1F34E); // Apple
+const allowedDirections = new Set(['ArrowRight', 'ArrowLeft', 'ArrowUp', 'ArrowDown']);
 
 
 function Field() {
 
     const [snakeHeadPosition, setSnakeHeadPosition] = useState({x: 0, y: 0});
     const [foodPosition, setFoodPosition] = useState({x: 5, y: 5});
+    const direction = useRef('ArrowRight');
 
     useEffect(() => {
         const interval = setInterval(() => {
-
             setSnakeHeadPosition(h => {
-                const newHead = {...h, x: h.x++};
-                newHead.x = (newHead.x < amountOfRows) ? newHead.x : 0
+
+                const newHead = {...h};
+
+                switch (direction.current) {
+                    case 'ArrowDown':
+                        newHead.x++;
+                        break
+                    case 'ArrowUp':
+                        newHead.x--;
+                        break
+                    case 'ArrowRight':
+                        newHead.y++;
+                        break
+                    case 'ArrowLeft':
+                        newHead.y--;
+                        break
+                    default:
+                        break
+                }
+
+                // Enable the snake head to go trough walls.
+                newHead.x = (newHead.x < amountOfRows) ? newHead.x : 0;
+                newHead.y = (newHead.y < amountOfColumns) ? newHead.y : 0;
+
                 return newHead;
             })
         }, 1000);
@@ -32,11 +55,19 @@ function Field() {
 
     const tiles = Array.from(Array(amountOfRows), () => Array.from(Array(amountOfColumns), () => {return {...defaultTileProps}}))
 
+    const handleKeyPress = (e) => {
+        if (allowedDirections.has(e.key)) {
+            direction.current = e.key;  // useState does not work in the setInterval.
+        }
+    };
+
     tiles[snakeHeadPosition.x][snakeHeadPosition.y].className = 'dark';
     tiles[foodPosition.x][foodPosition.y].isFood = true;
 
+    // The tabindex indicates that the element can be focussed. -1 forces the user to click the table first.
+    // TODO don't scroll the page when pressing ArrowUp or ArrowDown.
     return (
-        <table className='field'>
+        <table className='field' onKeyDown={handleKeyPress} tabIndex="-1">
             <tbody>
                 {tiles.map((row, rowIndex) => <tr key={rowIndex}>{row.map((tile, columnIndex) => <td key={columnIndex} className={tile.className}>{tile.isFood ? food : ''}</td>)}</tr>)}
             </tbody>
