aab0ea537f74ce0c97ab7f264caad12e86ff7760
Roel <roelwinter11@gmail.com>
2021-07-13 15:20:39 +0200

    Add button to start the game

--- src/components/SnakeGame/SnakeGame.js
+++ src/components/SnakeGame/SnakeGame.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './SnakeGame.css'
 import Field from '../Field/Field';
 import Scoreboard from '../ScoreBoard/Scoreboard';
 
 
 function SnakeGame() {
+
+    const [gameRunning, setGameRunning] = useState(false);
+
+    const handleClick = () => {
+        setGameRunning(true);
+    }
+
     return (
         <div className='snake-game'>
-            <Field/>
-            <Scoreboard/>
+            {gameRunning ? <> <Field/> <Scoreboard/> </> : <button onClick={handleClick}>Press me to start!</button>}
         </div>
     )
 }
