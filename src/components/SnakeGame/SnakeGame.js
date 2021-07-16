38d17ec51bc63809408d451a5507a2dcee4ef10c
Roel <roelwinter11@gmail.com>
2021-07-16 11:14:08 +0200

    Properly reset game when reset game button is pressed

--- src/components/SnakeGame/SnakeGame.js
+++ src/components/SnakeGame/SnakeGame.js
@@ -5,21 +5,23 @@ import Scoreboard from '../ScoreBoard/Scoreboard';
 
 const initialSnakeBody = [{x: 0, y: 5}, {x: 1, y: 5}];
 const initialFoodPosition = {x: 5, y: 5, icon: '🍎'};
+const initialPositions = {snakeBody: initialSnakeBody, food: initialFoodPosition};
 
 
 const calculateScore = (snakeBody) => {
     return (snakeBody.length - initialSnakeBody.length) * 10;
 }
 
+
 function SnakeGame() {
 
     const [gameRunning, setGameRunning] = useState(false);
-    const [positions, setPositions] = useState({snakeBody: initialSnakeBody, food: initialFoodPosition})
+    const [positions, setPositions] = useState(initialPositions)
 
     return (
         <div className='snake-game'>
             {gameRunning ? <Field positions={positions} setPositions={setPositions}/> : <button className='field' onClick={() => setGameRunning(true)}>Press me to start!</button>}
-            <Scoreboard score={calculateScore(positions.snakeBody)}buttonHandler={() => setGameRunning(false)}/>
+            <Scoreboard score={calculateScore(positions.snakeBody)} buttonHandler={() => {setGameRunning(false); setPositions(initialPositions)}}/>
         </div>
     )
 }
